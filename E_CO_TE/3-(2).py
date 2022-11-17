54a1cfd0e8e72d63c8e021b516d11b5f592e6242
kyunghoon <rlarudgns970@naver.com>
2022-11-17 10:47:12 +0900

    그리디_큰 수의 법칙

--- E_CO_TE/3-(2).py
+++ E_CO_TE/3-(2).py
@@ -1,9 +1,25 @@
-# 그리디
+# 그리디_큰 수의 법칙_간단 풀이
 
-# N, M, K 를 공백을 통해 입력받기
-N, M, K = map(int, input().split())
-# N개의 수를 공백으로 구분하여 입력받기
-data = list(map(int, input().split()))
+# n, m, k를 공백으로 구분하여 입력받기
+n, m, k = list(map(int, input().split()))
+# n개의 수를 공백으로 구분하여 입력받기
+data = list(map(int,input().split()))
 
-data.sort() # 입력받은 수들 정리하기
-print(data)
+data.sort() # 입력받은 수들을 정렬
+first = data[n - 1] # 가장 큰 수
+second = data[n - 2] # 두번째로 큰 수
+
+result = 0
+
+while True:
+    for i in range(k): # 가장 큰 수를 k번 더하기
+        if m == 0: # m이 0이라면 반복문을 탈출
+            break
+        result += first
+        m -= 1 # 더할때 마다 1씩 빼기
+    if m == 0: # m이 0이라면 반복문 탈출
+        break
+    result += second # 두번째로 큰 수를 한 번 더하기
+    m -= 1 # 더할 때마다 1씩 빼기
+
+print(result) # 최종 답안 출력
