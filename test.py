8ae22dba3613da6dbfa8e2c8295a370eb4b2e610
kyunghoon <rlarudgns970@naver.com>
2023-02-07 22:42:24 +0900

    .

--- test.py
+++ test.py
@@ -1,17 +1,2 @@
-n,k = map(int, input().split())
-total = 0
-
-while n != 1:
-    if n % k == 0:
-        n = n//k
-        total += 1
-        if n == 1:
-            print(total)
-            break
-        
-    else:
-        n -= 1
-        total += 1
-        if n == 1:
-            print(total)
-            break
+for i in range(8):
+    print(i)
