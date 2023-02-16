cd21861041bac2a6fa04b4872b6d8abd4b61d1ce
kyunghoon <rlarudgns970@naver.com>
2023-02-16 12:55:07 +0900

    .

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-# now = input()
-# row = int(now[1])
-# column = int(ord(now[0]))
+now = input()
+row = int(now[1])
+column = int(ord(now[0])) - int(ord('a')) + 1
 
-array = ['일','이','삼','사']
+steps = [(-2,-1), (-1,-2), (1,-2), (2,-1), (2,1), (1,2), (-1,2), (-2,1)]
 
-print(ord('일'))
+result = 0
+for step in steps:
+    next_row = row + step[0]
+    next_column = column + step[1]
+    # 해당위치로 이동이 가능하면 카운트 증가
+    if next_row>=1 and next_row<=8 and next_column>=1 and next_column<=8:
+        result += 1
+
+print(result)
