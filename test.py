2080c3483d866ab1d3010eaea8e6faf6401059f4
kyunghoon <rlarudgns970@naver.com>
2023-03-11 22:52:00 +0900

    .

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
-now = input()
-row = int(now[1])
-column = int(ord(now[0])) - int(ord('a')) + 1
+n, k = map(int, input().split())
+num = 0
 
-steps = [(-2,-1), (-1,-2), (1,-2), (2,-1), (2,1), (1,2), (-1,2), (-2,1)]
-
-result = 0
-for step in steps:
-    next_row = row + step[0]
-    next_column = column + step[1]
-    # 해당위치로 이동이 가능하면 카운트 증가
-    if next_row>=1 and next_row<=8 and next_column>=1 and next_column<=8:
-        result += 1
-
-print(result)
-
+while True:
+    if n % k == 0:
+        n = n // k
+        num += 1
+        if n == 1:
+            print(num)
+            break
+        
+    elif n % k != 0:
+        n -= 1
+        num += 1
+        if n == 1:
+            print(num)
+            break
