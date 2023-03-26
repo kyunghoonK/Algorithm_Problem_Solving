f8e72adae335792f9193dbe33deb388e739a2cdc
kyunghoon <rlarudgns970@naver.com>
2023-03-26 23:33:11 +0900

    .

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 from collections import deque
 
-# 큐 구현을 위해 deque 라이브러리를 사용
-queue = deque()
-
-queue.append(5)
-queue.append(2)
-queue.append(3)
-queue.append(7)
-queue.popleft()
-queue.append(1)
-queue.append(4)
-queue.popleft()
-
-print(queue)
+# BFS 메서드 정의
+def bfs(graph, start, visited):
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    
+    # 큐가 빌 때까지 반복
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
