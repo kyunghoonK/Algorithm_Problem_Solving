609cab0c876650ade2c541cd82318ca34f15a400
kyunghoon <rlarudgns970@naver.com>
2023-04-02 15:57:27 +0900

    .

--- test.py
+++ test.py
@@ -1,28 +1,10 @@
-n, m = map(int, input().split())
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-graph = []
+for i in range(len(array)):
+    min_index = i
+    for j in range(i, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
 
-for i in range(n):
-    graph.append(list(map(int, input().split())))
-    
-def dfs(x, y):
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
-    
-    if graph[x][y] == 0:
-        graph[x][y] = 1
-        
-        dfs(x-1, y)
-        dfs(x+1, y)
-        dfs(x, y-1)
-        dfs(x, y+1)
-        return True
-    return False
-
-result = 0
-for i in range(n):
-    for j in range(m):
-        if dfs(i, j) == True:
-            result += 1
-
-print(result)
+print(array) 
