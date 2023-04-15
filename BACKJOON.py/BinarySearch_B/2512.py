1e66f2595916bf8e990ae644ee352de56c0e8872
kyunghoon <rlarudgns970@naver.com>
2023-04-15 14:35:53 +0900

    .

---
+++ BACKJOON.py/BinarySearch_B/2512.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cities = list(map(int, input().split()))
+budget = int(input())
+
+start, end = 0, max(cities)
+
+# 이분 탐색
+
+while start <= end:
+    mid = (start + end) // 2
+    total = 0
+    for i in cities:
+        if i > mid:
+            total += mid
+        else:
+            total += i
+    if total <= budget:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
