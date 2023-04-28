01ed438879dbfb8ab432487a8639932feaa0d8b8
kyunghoon <rlarudgns970@naver.com>
2023-04-28 23:12:22 +0900

    .

--- E_CO_TE/BinarySearch/Q28.py
+++ E_CO_TE/BinarySearch/Q28.py
@@ -1,21 +1,21 @@
 # 고정점 찾기
-def binary_search(array, target, start, end):
+def binary_search(array, start, end):
     if start > end:
         return None
     mid = (start + end) // 2
-    if array[mid] == target:
+    if array[mid] == mid:
         return mid
-    elif array[mid] < target:
-        binary_search(array, target, mid+1, end)
+    elif array[mid] < mid:
+        binary_search(array, mid+1, end)
     else:
-        binary_search(array, target, start, mid-1)
+        binary_search(array, start, mid-1)
 
 n = int(input())
 data = list(map(int, input().split()))
 
-for i in data:
-    if binary_search(data, i, 0, len(data))!=None and binary_search(data, i, 0, len(data)) == data[binary_search(data, i, 0, len(data))]:
-        print(binary_search(data, i, 0, len(data)))
-    else:
-        print("-1")
-        
+index = binary_search(data, 0, n-1)
+
+if index == None:
+    print(-1)
+else:
+    print(index)
